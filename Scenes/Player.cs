8be2d6c5af50f3a9cf931f3e4bfa91deafbe7fe2
Edmier <kameron@edmier.com>
2022-01-29 12:32:55 -0500

    Commit .mono

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -6,9 +6,10 @@ public class Player : KinematicBody2D {
 
     public Vector2 velocity = new Vector2();
 
-    [Export] public int fallMultiplier = 2;
-    [Export] public int lowJumpMultiplier = 10;
-    [Export] public int jumpVelocity = 400;
+    [Export] public int FallMultiplier = 2;
+    [Export] public int LowJumpMultiplier = 10;
+    [Export] public int JumpVelocity = 400;
+    [Export] public int Gravity = 8;
 
     public void GetInput() {
         velocity = new Vector2();
@@ -24,6 +25,24 @@ public class Player : KinematicBody2D {
 
     public override void _PhysicsProcess(float delta) {
         GetInput();
+
+        velocity.y += Gravity;
+
+            //#Jump Physics
+        if (velocity.y > 0) { //#Player is falling
+            velocity += Vector2.UP * (-9.81) * (FallMultiplier); //#Falling action is faster than jumping action | Like in mario
+        }
+        else if (velocity.y < 0 && Input.IsActionJustReleased("jump")) { //#Player is jumping 
+            velocity += Vector2.UP * (-9.81) * (LowJumpMultiplier); //#Jump Height depends on how long you will hold key
+        }
+        if (IsOnFloor()) {
+            if (Input.IsActionJustPressed("jump")) { 
+                velocity = Vector2.UP * JumpVelocity; //#Normal Jump action
+            }
+        }
+
+        velocity = MoveAndSlide(velocity, Vector2(0,-1));
+
         velocity = MoveAndSlide(velocity);
     }
 

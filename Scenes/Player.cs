47b06f90bd346cf6ca465dc05dad0ae1d2e3bbcb
AddisonBD <89487478+AddisonBD@users.noreply.github.com>
2022-01-29 16:56:20 -0500

    End Screen Basic Setup

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -10,11 +10,11 @@ public class Player : KinematicBody2D {
 	[Export] public int LowJumpMultiplier = 10;
 	[Export] public int JumpVelocity = 400;
 
-    [Export] public int GravityEffect = 2;
-    [Export] public float GRAVITY = 9.81f;
+	[Export] public int GravityEffect = 2;
+	[Export] public float GRAVITY = 9.81f;
 
 	public void GetInput() {
-        velocity = new Vector2();
+		velocity = new Vector2();
 
 		velocity.y += GravityEffect / 0.5f;
 
@@ -22,20 +22,20 @@ public class Player : KinematicBody2D {
 		if (velocity.y > 0) { //#Player is falling
 			velocity += new Vector2(0, -1 * (-GRAVITY) * (FallMultiplier)); //#Falling action is faster than jumping action | Like in mario
 		} else if (velocity.y < 0 && Input.IsActionJustReleased("jump")) { //#Player is jumping 
-            Console.WriteLine("nice");
+			Console.WriteLine("nice");
 			velocity += new Vector2(0, -1 * (GRAVITY) * (LowJumpMultiplier)); //#Jump Height depends on how long you will hold key
 		}
 
 		velocity = MoveAndSlide(velocity, new Vector2(0,-1));
 
-        if (IsOnFloor()) {
-            // Console.WriteLine("floor");
+		if (IsOnFloor()) {
+			// Console.WriteLine("floor");
 			if (Input.IsActionJustPressed("jump")) { 
 				velocity = new Vector2(0, -1 * JumpVelocity); //#Normal Jump action
 			}
 		}
 
-        velocity = MoveAndSlide(velocity);
+		velocity = MoveAndSlide(velocity);
 	}
 	public override void _PhysicsProcess(float delta) {
 		GetInput();

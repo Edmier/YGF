f8e74b1b92858f4fb91dc5d12efe71cf5e78814b
Edmier <kameron@edmier.com>
2022-01-29 15:44:37 -0500

    lil hop

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -10,40 +10,35 @@ public class Player : KinematicBody2D {
 	[Export] public int LowJumpMultiplier = 10;
 	[Export] public int JumpVelocity = 400;
 
-    [Export] public int Gravity = 8;
+    [Export] public int GravityEffect = 2;
+    [Export] public float GRAVITY = 9.81f;
 
 	public void GetInput() {
-		velocity = new Vector2();
+        velocity = new Vector2();
 
-		if (Input.IsActionJustPressed("jump"))
-			jump();
-
-		if (Input.IsActionJustReleased("jump"))
-			jumpStop();
-
-		velocity = velocity.Normalized() * speed;
-	}
-	public override void _PhysicsProcess(float delta) {
-		GetInput();
-
-		velocity.y += Gravity;
+		velocity.y += GravityEffect / 0.5f;
 
 		// 	//#Jump Physics
-		// if (velocity.y > 0) { //#Player is falling
-		// 	velocity += Vector2.up * (-9.81) * (FallMultiplier); //#Falling action is faster than jumping action | Like in mario
-		// }
-		// else if (velocity.y < 0 && Input.IsActionJustReleased("jump")) { //#Player is jumping 
-		// 	velocity += Vector2.up * (-9.81) * (LowJumpMultiplier); //#Jump Height depends on how long you will hold key
-		// }
-		// if (IsOnFloor()) {
-		// 	if (Input.IsActionJustPressed("jump")) { 
-		// 		velocity = Vector2.up * JumpVelocity; //#Normal Jump action
-		// 	}
-		// }
+		if (velocity.y > 0) { //#Player is falling
+			velocity += new Vector2(0, -1 * (-GRAVITY) * (FallMultiplier)); //#Falling action is faster than jumping action | Like in mario
+		} else if (velocity.y < 0 && Input.IsActionJustReleased("jump")) { //#Player is jumping 
+            Console.WriteLine("nice");
+			velocity += new Vector2(0, -1 * (GRAVITY) * (LowJumpMultiplier)); //#Jump Height depends on how long you will hold key
+		}
 
 		velocity = MoveAndSlide(velocity, new Vector2(0,-1));
 
-		velocity = MoveAndSlide(velocity);
+        if (IsOnFloor()) {
+            // Console.WriteLine("floor");
+			if (Input.IsActionJustPressed("jump")) { 
+				velocity = new Vector2(0, -1 * JumpVelocity); //#Normal Jump action
+			}
+		}
+
+        velocity = MoveAndSlide(velocity);
+	}
+	public override void _PhysicsProcess(float delta) {
+		GetInput();
 	}
 
 	public void jump() {

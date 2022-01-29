e9ac58095b38b7c183beeea3b42ad61ac168ec47
AddisonBD <89487478+AddisonBD@users.noreply.github.com>
2022-01-29 12:12:14 -0500

    bug fix

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -2,32 +2,32 @@ using Godot;
 using System;
 
 public class Player : KinematicBody2D {
-    [Export] public int speed = 200;
+	[Export] public int speed = 200;
 
-     public Vector2 velocity = new Vector2();
+	 public Vector2 velocity = new Vector2();
 
-    public void GetInput() {
-        velocity = new Vector2();
+	public void GetInput() {
+		velocity = new Vector2();
 
-        if (Input.IsActionJustPressed("jump"))
-            jump();
+		if (Input.IsActionJustPressed("jump"))
+			jump();
 
-        if (Input.IsActionJustReleased("jump"))
-            jumpStop();
+		if (Input.IsActionJustReleased("jump"))
+			jumpStop();
 
-        velocity = velocity.Normalized() * speed;
-    }
+		velocity = velocity.Normalized() * speed;
+	}
 
-    public override void _PhysicsProcess(float delta) {
-        GetInput();
-        velocity = MoveAndSlide(velocity);
-    }
+	public override void _PhysicsProcess(float delta) {
+		GetInput();
+		velocity = MoveAndSlide(velocity);
+	}
 
-    public void jump() {
+	public void jump() {
 
-    }
+	}
 
-    public void jumpCut() {
-        
-    }
+	public void jumpCut() {
+		
+	}
 }

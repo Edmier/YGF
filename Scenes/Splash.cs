692fdfe8122d86896cb2dc1239bef24bc6b9fb7f
Edmier <kameron@edmier.com>
2022-01-30 00:22:00 -0500

    Better Splash

--- Scenes/Splash.cs
+++ Scenes/Splash.cs
@@ -4,16 +4,32 @@ using System;
 public class Splash : Node
 {
     Sprite _img;
+    ColorRect _color;
+
+    private bool appearing = true;
+
     public override void _Ready() {
         GetNode<CanvasLayer>("CanvasLayer").Layer = 2;
+
         _img = GetNode<Sprite>("CanvasLayer/ColorRect/Sprite");
         _img.Modulate = new Color(1, 1, 1, 0.1f);
+
+        _color = GetNode<ColorRect>("CanvasLayer/ColorRect");
     }
 
     public override void _Process(float delta) {
-        if (_img.Modulate.a < 1) {
-            Color color = new Color(1, 1, 1, _img.Modulate.a + 0.005f);
+        if (appearing) {
+            float Opacity = _img.Modulate.a + 0.005f;
+            Color color = new Color(1, 1, 1, Opacity);
             _img.Modulate = color;
+
+            appearing = !(Opacity >= 1);
+        } else if (_img.Modulate.a > 0) {
+            Color color = new Color(1, 1, 1, _img.Modulate.a - 0.02f);
+            _img.Modulate = color;
+            _color.Color = color;
+        } else {
+            QueueFree();
         }
     }
 

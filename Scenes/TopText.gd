f2c37e9c6223a8bbf801d42ecd2769053c757108
AddisonBD <89487478+AddisonBD@users.noreply.github.com>
2022-01-29 19:21:24 -0500

    Changing Text Implemented

---
+++ Scenes/TopText.gd
@@ -0,0 +1,38 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+var x = 0;
+
+func _on_Timer_timeout():
+	if (x == 0): 
+		set_text("He told me.")
+		x = x + 1
+	elif (x == 1): 
+		set_text("Of life.")
+		x = x + 1
+	elif (x == 2): 
+		set_text("Of fear.")
+		x = x + 1
+	elif (x == 3): 
+		set_text("Of bravery.")
+		x = x + 1
+	elif (x == 4): 
+		set_text("You'll go far.")
+		x = x + 1
+	
+	
+	

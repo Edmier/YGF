2b417bedc554a3dbcf4b08a0d9833407c61e684d
Edmier <kameron@edmier.com>
2022-01-29 20:38:52 -0500

    general background music

--- Scenes/Obstacle.cs
+++ Scenes/Obstacle.cs
@@ -8,6 +8,7 @@ public class Obstacle : RigidBody2D
 
 	public void init(int ScrollSpeed) {
 		this.ScrollSpeed = ScrollSpeed;
+        Position = new Vector2(600, 400);
 	}
 
 	public override void _Ready() {
@@ -15,7 +16,7 @@ public class Obstacle : RigidBody2D
 	}
 
 	public override void _PhysicsProcess(float delta) {
-		// Position.x -= (ScrollSpeed / 100 * delta);
+		Position -= new Vector2(ScrollSpeed * delta, 0);
 
 		if (Position.x < -100) {
 			QueueFree();
